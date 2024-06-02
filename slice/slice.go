dc8982b2165aa706e31f91383fa148035a68ab83
Durgesh Thakur <durgesh.bisen10102@gmail.com>
2024-06-02 10:01:03 +0530

    add slice

--- slice/slice.go
+++ slice/slice.go
@@ -1,9 +1,19 @@
 package main
+
 import "fmt"
 
-func main(){
-    var slice[] string
-	
+func main() {
+	fmt.Println("hello this is a slice")
+	// number := []int{1, 2, 3, 4, 5}
+	// number = append(number, 6, 7, 8, 9)
+	// fmt.Println("the number is", number)
 
-	fmt.Println("hello this is a slice");
+	// ---------------- crate using make funk ------------------
+
+	numbers := make([]int, 0)
+	numbers = append(numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	fmt.Println("number is", numbers)
+	fmt.Println("length is", len(numbers))
+	fmt.Println("capacity is", cap(numbers))
+
 }
